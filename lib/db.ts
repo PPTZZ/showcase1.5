486fa7776ddaa3908261fcb519c73a92bf14b897
Alex Poputa <poputa.alex@gmail.com>
2025-06-13 15:38:31 +0300

    changed mongoDB URI

--- lib/db.ts
+++ lib/db.ts
@@ -6,7 +6,7 @@ async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(process.env.DB_CONNECTION!);
+  const db = await mongoose.connect(process.env.MONGODB_URI!);
 
   connection.isConnected = db.connections[0].readyState;
   console.log(`[server]: Connected to database`);
